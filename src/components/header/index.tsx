3ac4b9ebcf83d2b3b1dcdb244de38d8e8c3a3c9d
rtrindade08 <renata.trindade.ferreira@gmail.com>
2021-03-09 15:44:00 +0000

    started the header and added reactstrap

---
+++ src/components/header/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+} from "reactstrap";
+export const Header: React.FC = () => {
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler />
+        <Collapse navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
---
+++ src/screens/home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Header } from "../components/header";
+export const Home: React.FC = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
